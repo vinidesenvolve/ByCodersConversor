ec7496a9885be3c4f8f0a41d20710e548b2c4bd0
vinidesenvolve <vinidesenvolve@gmail.com>
2021-08-30 22:39:49 -0300

    Anotaçao CORS adicionada para testar frontend

--- src/main/java/br/com/bycoders/conversor/controller/TransacaoController.java
+++ src/main/java/br/com/bycoders/conversor/controller/TransacaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,11 @@ import br.com.bycoders.conversor.service.TransacaoService;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "http://localhost:4200")
 public class TransacaoController {
     
     @Autowired
-    TransacaoService transacaoService;
+    private TransacaoService transacaoService;
 
     @PostMapping("/upload")
     public ResponseEntity<String> upload(@RequestParam MultipartFile arquivo) {
